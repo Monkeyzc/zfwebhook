2173a34f6ecd627f398278af60f6737f9410e263
ZhaoFeiK <zhaofei199210@gmail.com>
2018-10-10 12:12:08 +0800

    send issue to ding talk

--- index.js
+++ index.js
@@ -6,9 +6,60 @@ var request = require('request')
 var app = express()
 
 const port = process.env.PORT || 8080
+const DingTalkWebhook = 'https://oapi.dingtalk.com/robot/send?access_token=f8d55d2ea46ff45a96db7da30ff73a8976734b8fe43f117bac0d302c9f4eccdd'
+const atMobiles = ['13051149394']
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
+
+/**
+bug 推送消息内容
+  {
+    "event": "issue_impact_change",
+    "payload_type": "issue",
+    "payload": {
+      "display_id": 123 ,
+      "title": "Issue Title" ,
+      "method": "methodName of issue",
+      "impact_level": 2,
+      "crashes_count": 54,
+      "impacted_devices_count": 16,
+      "url": "http://crashlytics.com/full/url/to/issue"
+    }
+  }
+*/
+var sendIssueToDingTalk = function (payload, callback) {
+  var markdown = {
+    title: 'displayName iOS 测试包',
+    text: `#### Title: ${payload.title} \n > 1. method: ${payload.method}\n > 2. impact_level: ${payload.impact_level}\n > 3. crashes_count: ${payload.crashes_count}\n >4. impacted_devices_count: ${payload.impacted_devices_count} > 4. [url](${payload.url})\n 
+        @13051149394
+        `
+  }
+
+  var dingtalkData = {
+     "msgtype": "markdown",
+     "markdown": markdown,
+      "at": {
+         "atMobiles": atMobiles, 
+         "isAtAll": false
+     }
+  }
+  var dingtalkOptions = {
+    url: DingTalkWebhook,
+    headers: {'Content-Type': 'application/json ;charset=utf-8'},
+    method: 'POST',
+    json: true,
+    body: dingtalkData
+  }
+
+  request(dingtalkOptions, function (err, httpResponse, body) {
+    console.log(err)
+    console.log(httpResponse)
+    console.log(body)
+    callback()
+  })
+}
+
 
 app.post('', function (req, res) {
   console.log(req.body)
@@ -17,7 +68,9 @@ app.post('', function (req, res) {
   if (body.event && body.event === 'verification') {
     res.send({mes: 'verification post'})
   } else if (body.event && body.event === 'issue_impact_change') {
-    res.send({mes: 'issue_impact_change post'})
+    sendIssueToDingTalk(body.payload, function () {
+      res.send({mes: 'issue_impact_change post'})
+    })
   } else {
     res.send({mes: 'default post'})
   }

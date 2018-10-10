554a2cbefaf8167688531756c5ed751a80caa314
ZhaoFeiK <zhaofei199210@gmail.com>
2018-10-10 11:00:29 +0800

    create server, listen default post request

---
+++ index.js
@@ -0,0 +1,30 @@
+
+var express = require('express')
+var bodyParser = require('body-parser')
+var request = require('request')
+
+var app = express()
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+app.post('', function (req, res) {
+  console.log(req.body)
+
+  var body = req.body
+  if (body.event && body.event === 'verification') {
+    res.send({mes: 'verification post'})
+  } else if (body.event && body.event === 'issue_impact_change') {
+    res.send({mes: 'issue_impact_change post'})
+  } else {
+    res.send({mes: 'default post'})
+  }
+})
+
+app.get('/test', function (req, res) {
+  res.send({mes: 'hello world'})
+})
+
+app.listen('8080', function (req, res) {
+  console.log('server is listening on 8080')
+})
